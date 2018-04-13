963ed9067ad99fef9813a1e44012efafda5b4321
Jake Goldman <jgoldman@stuy.edu>
2018-04-13 14:05:50 -0400

    added old python reader

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+from csv import DictReader
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def graph():
-    return render_template('homepage.html')
+    # GET LOAD THE DATA FROM A CSV
+    data = []
+    with open( "data/GDP2014.csv") as file:
+        reader = DictReader( file )
+        for row in reader:
+            data.append( row )
+    # print data
+
+    return render_template('homepage.html', data=data)
 
 if __name__ == "__main__":
     app.debug = True
